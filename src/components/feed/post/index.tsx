c22644ccb3e649b3420a5e091340446d3595a950
luCAOrx <lucaorx@protonmail.com>
2026-03-26 11:33:58 -0300

    chore: Add object with the components from post
    An object was added containing the components that are part of the post following the composition pattern

---
+++ src/components/feed/post/index.tsx
@@ -0,0 +1,9 @@
+import { PostContent } from "./post-content"
+import { PostInput } from "./post-input"
+import { PostRoot } from "./post-root"
+
+export const Post = {
+  Root: PostRoot,
+  Input: PostInput,
+  Content: PostContent,
+}
