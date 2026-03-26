35e2ee27ff0ad5302738bf169102d15cfc7e63b9
luCAOrx <lucaorx@protonmail.com>
2026-03-26 12:05:08 -0300

    chore: Add home page

---
+++ src/pages/home-page.tsx
@@ -0,0 +1,13 @@
+import { Header } from "@/components/header/header"
+import { Footer } from "@/components/footer/footer"
+import { Feed } from "@/components/feed/feed"
+
+export function HomePage() {
+  return (
+    <>
+      <Header />
+      <Feed />
+      <Footer />
+    </>
+  )
+}
