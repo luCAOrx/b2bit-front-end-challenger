afcf64fa08133ea75a0790b7d01ece21a3e4ac75
luCAOrx <lucaorx@protonmail.com>
2026-03-26 11:51:18 -0300

    chore: Add page not found component

---
+++ src/components/page-not-found/page-not-found.tsx
@@ -0,0 +1,7 @@
+export function PageNotFound() {
+  return (
+    <div className="flex min-h-svh items-center justify-center text-3xl font-black text-text-primary">
+      Erro 404: Página não encontrada
+    </div>
+  )
+}
