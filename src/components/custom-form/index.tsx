5cf272dc91f327b323ff146556f2bf753ba375af
luCAOrx <lucaorx@protonmail.com>
2026-03-31 23:41:47 -0300

    chore: Add header, form and footer components

--- src/components/custom-form/index.tsx
+++ src/components/custom-form/index.tsx
@@ -1,5 +1,11 @@
+import { CustomFormFooter } from "./custom-form-footer"
+import { CustomFormForm } from "./custom-form-form"
+import { CustomFormHeader } from "./custom-form-header"
 import { CustomFormRoot } from "./custom-form-root"
 
 export const CustomForm = {
   Root: CustomFormRoot,
+  Header: CustomFormHeader,
+  Form: CustomFormForm,
+  Footer: CustomFormFooter,
 }
