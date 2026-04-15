b3e8458513a2c8a7b46c88e917d78e51fa40ead2
luCAOrx <lucaorx@protonmail.com>
2026-04-15 06:02:50 -0300

    refactor: Remove fragment component

--- src/components/custom-input/custom-input-group-addon.tsx
+++ src/components/custom-input/custom-input-group-addon.tsx
@@ -9,15 +9,13 @@ export function CustomInputGroupAddon({
   children,
 }: CustomInputGroupAddonProperties) {
   return (
-    <>
-      <InputGroupAddon
-        align="inline-end"
-        className="flex h-14.25 w-14 items-center justify-center"
-      >
-        <div className="group h-6 min-h-6 w-6 min-w-6 rounded-[8px]">
-          {children}
-        </div>
-      </InputGroupAddon>
-    </>
+    <InputGroupAddon
+      align="inline-end"
+      className="flex h-14.25 w-14 items-center justify-center"
+    >
+      <div className="group h-6 min-h-6 w-6 min-w-6 rounded-[8px]">
+        {children}
+      </div>
+    </InputGroupAddon>
   )
 }
