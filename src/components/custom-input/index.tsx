9bb5a9670af859da837becda9d55f5fc6d87336b
luCAOrx <lucaorx@protonmail.com>
2026-04-15 09:21:32 -0300

    chore: Add Label, TextArea, Container and Error properties

--- src/components/custom-input/index.tsx
+++ src/components/custom-input/index.tsx
@@ -1,9 +1,17 @@
 import { CustomInputGroupAddon } from "./custom-input-group-addon"
 import { CustomInputGroupInput } from "./custom-input-group-input"
+import { CustomInputGroupInputContainer } from "./custom-input-group-input-container"
+import { CustomInputGroupInputFieldError } from "./custom-input-group-input-field-error"
+import { CustomInputGroupInputLabel } from "./custom-input-group-input-label"
 import { CustomInputGroupInputRoot } from "./custom-input-group-root"
+import { CustomInputGroupTextArea } from "./custom-input-group-text-area"
 
 export const CustomInputGroup = {
   Root: CustomInputGroupInputRoot,
+  Label: CustomInputGroupInputLabel,
   Input: CustomInputGroupInput,
+  TextArea: CustomInputGroupTextArea,
+  Container: CustomInputGroupInputContainer,
+  Error: CustomInputGroupInputFieldError,
   Addon: CustomInputGroupAddon,
 }
