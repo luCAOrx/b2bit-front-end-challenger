755c1f019b8353f46c112cae415f360a4762a0d4
luCAOrx <lucaorx@protonmail.com>
2026-04-15 05:21:07 -0300

    refactor: Add Label, Container, Addon and Error CustomInputGroup properties inside CustomInputGroup.Root property

--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -8,6 +8,8 @@ import { useAuth } from "@/hooks/use-auth"
 import { LoginUserFormSchema } from "@/form-schemas/login-user-form-schema"
 import { CustomInputGroup } from "../custom-input"
 import { CustomForm } from "../custom-form"
+import { Field } from "../ui/field"
+import SubmitLoadingButton from "../submit-loading-button"
 
 export function Login() {
   const [showPassword, setShowPassword] = useState(false)
@@ -51,21 +53,25 @@ export function Login() {
           control={form.control}
           render={({ field, fieldState }) => (
             <CustomInputGroup.Root fieldState={fieldState}>
-              <CustomInputGroup.Input
-                field={field}
-                fieldState={fieldState}
+              <CustomInputGroup.Label
                 form={form}
                 formFieldName="email"
                 fieldLabelTitle="E-mail"
-                reactComponentInputProperties={{
-                  type: "email",
-                  placeholder: "Insira o seu e-mail",
-                }}
-              >
-                <CustomInputGroup.Addon>
-                  <Mail className="h-6 min-h-6 w-6 min-w-6 text-input-placeholder not-dark:text-text-secondary" />
-                </CustomInputGroup.Addon>
-              </CustomInputGroup.Input>
+              />
+              <CustomInputGroup.Container form={form} formFieldName="email">
+                <CustomInputGroup.Input
+                  field={field}
+                  formFieldName="email"
+                  type="email"
+                  inputMode="email"
+                  placeholder="Insira o seu e-mail"
+                >
+                  <CustomInputGroup.Addon>
+                    <Mail className="h-6 min-h-6 w-6 min-w-6 text-input-placeholder not-dark:text-text-secondary" />
+                  </CustomInputGroup.Addon>
+                </CustomInputGroup.Input>
+              </CustomInputGroup.Container>
+              <CustomInputGroup.Error fieldState={fieldState} />
             </CustomInputGroup.Root>
           )}
         />
@@ -74,33 +80,46 @@ export function Login() {
           control={form.control}
           render={({ field, fieldState }) => (
             <CustomInputGroup.Root fieldState={fieldState}>
-              <CustomInputGroup.Input
-                fieldState={fieldState}
+              <CustomInputGroup.Label
                 form={form}
-                fieldLabelTitle="Senha"
                 formFieldName="password"
-                field={field}
-                reactComponentInputProperties={{
-                  type: showPassword ? "text" : "password",
-                  placeholder: "Insira a sua senha",
-                }}
-              >
-                <CustomInputGroup.Addon>
-                  <InputGroupButton
-                    onClick={togglePasswordVisibility}
-                    className="group h-6 min-h-6 w-6 min-w-6 cursor-pointer rounded-[8px] border-none bg-transparent! shadow-none hover:bg-transparent!"
-                  >
-                    {showPassword ? (
-                      <EyeOffIcon className="group-hover:fill-text-text-secondary-foreground/30! group-hover:text-text-text-secondary-foreground/30! h-6 min-h-6 w-6 min-w-6 text-input-placeholder transition-colors not-dark:text-text-secondary not-dark:placeholder-input-placeholder group-hover:text-text-secondary-foreground/30!" />
-                    ) : (
-                      <EyeIcon className="group-hover:fill-text-text-secondary-foreground/30! group-hover:text-text-text-secondary-foreground/30! h-6 min-h-6 w-6 min-w-6 text-input-placeholder transition-colors not-dark:text-text-secondary not-dark:placeholder-input-placeholder group-hover:text-text-secondary-foreground/30!" />
-                    )}
-                  </InputGroupButton>
-                </CustomInputGroup.Addon>
-              </CustomInputGroup.Input>
+                fieldLabelTitle="Senha"
+              />
+              <CustomInputGroup.Container form={form} formFieldName="email">
+                <CustomInputGroup.Input
+                  formFieldName="password"
+                  field={field}
+                  type={showPassword ? "text" : "password"}
+                  inputMode="text"
+                  placeholder="Insira a sua senha"
+                >
+                  <CustomInputGroup.Addon>
+                    <InputGroupButton
+                      onClick={togglePasswordVisibility}
+                      className="group h-6 min-h-6 w-6 min-w-6 cursor-pointer rounded-[8px] border-none bg-transparent! shadow-none hover:bg-transparent!"
+                    >
+                      {showPassword ? (
+                        <EyeOffIcon className="group-hover:fill-text-text-secondary-foreground/30! group-hover:text-text-text-secondary-foreground/30! h-6 min-h-6 w-6 min-w-6 text-input-placeholder transition-colors not-dark:text-text-secondary not-dark:placeholder-input-placeholder group-hover:text-text-secondary-foreground/30!" />
+                      ) : (
+                        <EyeIcon className="group-hover:fill-text-text-secondary-foreground/30! group-hover:text-text-text-secondary-foreground/30! h-6 min-h-6 w-6 min-w-6 text-input-placeholder transition-colors not-dark:text-text-secondary not-dark:placeholder-input-placeholder group-hover:text-text-secondary-foreground/30!" />
+                      )}
+                    </InputGroupButton>
+                  </CustomInputGroup.Addon>
+                </CustomInputGroup.Input>
+              </CustomInputGroup.Container>
+              <CustomInputGroup.Error fieldState={fieldState} />
             </CustomInputGroup.Root>
           )}
         />
+
+        <Field orientation="horizontal">
+          <SubmitLoadingButton
+            size="xs"
+            isSubmitting={form.formState.isSubmitting}
+          >
+            Continuar
+          </SubmitLoadingButton>
+        </Field>
       </CustomForm.Form>
       <CustomForm.Footer />
     </CustomForm.Root>
