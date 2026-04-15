275f57d7fcdd054ef3f51d759a85732f168eb8dd
luCAOrx <lucaorx@protonmail.com>
2026-04-15 09:08:52 -0300

    chore: Add variant variable and put it to CustomInputGroupInputRoot properties. Add VariantProps<typeof customInputGroupInputRootVariants> type to CustomInputGroupInputRoot function. Add classname to Field component

--- src/components/custom-input/custom-input-group-root.tsx
+++ src/components/custom-input/custom-input-group-root.tsx
@@ -1,15 +1,38 @@
 import { Field } from "../ui/field"
 import type { ControllerFieldState } from "react-hook-form"
 import type { ReactNode } from "react"
+import { cva, type VariantProps } from "class-variance-authority"
+import { cn } from "@/lib/utils"
 
 interface CustomInputGroupInputRootProperties {
   children: ReactNode
   fieldState: ControllerFieldState
 }
 
+const customInputGroupInputRootVariants = cva("", {
+  variants: {
+    variant: {
+      default: "",
+      post: "border-b not-dark:shadow-md",
+    },
+  },
+  defaultVariants: {
+    variant: "default",
+  },
+})
+
 export function CustomInputGroupInputRoot({
   children,
   fieldState,
-}: CustomInputGroupInputRootProperties) {
-  return <Field data-invalid={fieldState.invalid}>{children}</Field>
+  variant = "default",
+}: CustomInputGroupInputRootProperties &
+  VariantProps<typeof customInputGroupInputRootVariants>) {
+  return (
+    <Field
+      className={cn(customInputGroupInputRootVariants({ variant }))}
+      data-invalid={fieldState.invalid}
+    >
+      {children}
+    </Field>
+  )
 }
